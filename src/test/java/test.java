a4e4ddbe2d3271c13ed1c77716a4650959ec6da6
Muath Ibrahim <muath@Muaths-MacBook-Air.local>
2025-09-18 20:28:39 +1000

    md

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class SimpleTest {
-    
-@Test
-void calculator(){
 
-    assertEquals(4, 2 + 2);
-}
+	@Test
+	void calculator() {
+
+		assertEquals(4, 2 + 2);
+	}
+
 }
